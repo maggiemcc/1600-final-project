0f93c1549fc6eaad276a64c2af28bb78c826a031
maggiemcc <maggiemccausland@hotmail.com>
2019-10-23 18:31:59 -0600

    males and females visible only on their button

--- js/starwars.js
+++ js/starwars.js
@@ -2,10 +2,11 @@ import { films } from '../Assets/films.js'
 import { people } from '../Assets/people.js'
 console.log(films[0].opening_crawl)
 
+let mainHeader = document.querySelector('header')
 let mainArea = document.querySelector('main')
 
 
-films.forEach(function(film) {
+/* films.forEach(function(film) {
 let filmDiv = document.createElement('div')
 let title = document.createElement('h1')
 let crawl = document.createElement('p')
@@ -17,9 +18,9 @@ title.textContent = film.title
 crawl.innerText = film.opening_crawl
     
 mainArea.appendChild(filmDiv)
-  });
+  }); */
 
-
+  
 people.forEach(function(person) {
     let personDiv = document.createElement('div')
     let name = document.createElement('h1')
@@ -50,10 +51,60 @@ function getCharNumber(charURL) {
   }
 }
 
-
 const maleCharacters = people.filter(person => person.gender === 'male')
-console.log(maleCharacters)
 const femaleCharacters = people.filter(person => person.gender === 'female')
+const otherCharacters = people.filter(person => person.gender !== 'female' & person.gender !== 'male')
+console.log(otherCharacters)
+const allDivs = Array.from(mainArea.querySelectorAll('div'))
+
+let maleButton = document.createElement('button')
+maleButton.textContent = "Male Characters"
+maleButton.addEventListener('click', () => {
+    maleCharacters.forEach(elt => {
+    let matchedDiv = allDivs.filter(element => {
+        return element.firstChild.textContent === elt.name
+    })
+      console.log(matchedDiv)
+      matchedDiv[0].setAttribute("style", "visibility: visibile;")
+  })
+  femaleCharacters.forEach(elt => {  
+    let matchedDiv = allDivs.filter(element => {
+        return element.firstChild.textContent === elt.name
+    })
+      console.log(matchedDiv)
+      matchedDiv[0].setAttribute("style", "display: none;")
+  })
+})
+
+let femaleButton = document.createElement('button')
+femaleButton.textContent = "Female Characters"
+femaleButton.addEventListener('click', event => {
+femaleCharacters.forEach(elt => {
+    let matchedDiv = allDivs.filter(element => {
+        return element.firstChild.textContent === elt.name
+    })
+      console.log(matchedDiv)
+      matchedDiv[0].setAttribute("style", "visibility: visibile;")
+  })
+  maleCharacters.forEach(elt => {  
+    let matchedDiv = allDivs.filter(element => {
+        return element.firstChild.textContent === elt.name
+    })
+      console.log(matchedDiv)
+      matchedDiv[0].setAttribute("style", "display: none;")
+  })
+});
+
+
+
+mainHeader.appendChild(maleButton)
+mainHeader.appendChild(femaleButton)
+
+
+
+
+
+console.log(maleCharacters)
 console.log(femaleCharacters)
 
 

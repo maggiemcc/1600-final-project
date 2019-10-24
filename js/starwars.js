4dc88250ad1c31090160c817e532d8fdff4b7bbb
maggiemcc <maggiemccausland@hotmail.com>
2019-10-23 19:03:49 -0600

    turned film into comment

--- js/starwars.js
+++ js/starwars.js
@@ -6,7 +6,7 @@ let mainHeader = document.querySelector('header')
 let mainArea = document.querySelector('main')
 
 
-films.forEach(function(film) {
+/* films.forEach(function(film) {
 let filmDiv = document.createElement('div')
 let title = document.createElement('h1')
 let crawl = document.createElement('p')
@@ -18,7 +18,7 @@ title.textContent = film.title
 crawl.innerText = film.opening_crawl
     
 mainArea.appendChild(filmDiv)
-  });
+  }); */
 
   
 people.forEach(function(person) {

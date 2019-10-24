ec7dd527f870395d0639b2ae36dea205e921b83e
maggiemcc <maggiemccausland@hotmail.com>
2019-10-24 11:14:28 -0600

    added ship photos

--- js/starships.js
+++ js/starships.js
@@ -1,1 +1,27 @@
 import {starships } from '../Assets/starships.js'
+console.log(starships)
+
+let contentArea = document.querySelector('.content')
+starships.forEach(ship => {
+    let shipDiv = document.createElement('div')
+    let shipName = document.createElement('h3')
+    let shipPic = document.createElement('img')
+
+    shipName.textContent = ship.name
+    let shipNum= getCharNumber(ship.url)
+    shipPic.src = 'https://starwards-visualguide.com/assets/img/starships/$(shipNum).jpg'
+    
+shipDiv.appendChild(shipName)
+    shipDiv.appendChild(shipPic)
+    contentArea.appendChild(shipDiv)
+})
+
+function getCharNumber(charURL) {
+    let end = charURL.lastIndexOf('/')
+    let charID = charURL.substring(end -2, end)
+    if(charID.indexOf('/') !== -1 ) {
+      return charID.slice(1,2)
+    } else {
+      return charID
+    }
+  }

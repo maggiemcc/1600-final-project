f2923c26367a8b33dffa14cb51e927f9f27b24dd
maggiemcc <maggiemccausland@hotmail.com>
2019-10-29 10:00:59 -0600

    reverted back

--- js/final.js
+++ js/final.js
@@ -1,3 +1,32 @@
+import {starships } from '../Assets/starships.js'
+console.log(starships)
+
+let contentArea = document.querySelector('.content')
+starships.forEach(ship => {
+    let shipDiv = document.createElement('div')
+    let shipName = document.createElement('h3')
+    let shipPic = document.createElement('img')
+
+    shipName.textContent = ship.name
+    let shipNum= getCharNumber(ship.url)
+    shipPic.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    
+shipDiv.appendChild(shipName)
+    shipDiv.appendChild(shipPic)
+    contentArea.appendChild(shipDiv)
+})
+
+function getCharNumber(charURL) {
+  let end = charURL.lastIndexOf('/')
+  let charID = charURL.substring(end -2, end)
+  if(charID.indexOf('/') !== -1 ) {
+    return charID.slice(1,2)
+  } else {
+    return charID
+  }
+}
+
+
 let myPic = document.createElement('img')
-myPic.setAttribute("src", 'https://lord-of-the-rings.org/books/Aragorn.jpg')
+myPic.setAttribute("src", 'http://www.iment.com/maida/tv/lordoftherings/gandalf-small.jpg')
 document.body.appendChild(myPic)

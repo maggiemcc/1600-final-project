d1b2c4e817aa3647d6f9f5cffaf87a6974fce042
maggiemcc <maggiemccausland@hotmail.com>
2019-10-29 11:55:38 -0600

    commented out text on baseball js

--- js/baseball.js
+++ js/baseball.js
@@ -7,24 +7,24 @@ console.log(JSON.stringify(myJson))
 
 console.log(allPokemon().then)
 
-/*people.forEach(function(person) {
-    let personDiv = document.createElement('div')
+/*allPokemon.forEach(function(poke) {
+    let pokeDiv = document.createElement('div')
     let name = document.createElement('h1')
-    let gender = document.createElement('h3')
+    let type = document.createElement('h3')
     let pic = document.createElement('img')
 
-    personDiv.appendChild(name)
-    personDiv.appendChild(gender)
-    personDiv.appendChild(pic)
+    pokeDiv.appendChild(name)
+    pokeDiv.appendChild(type)
+    pokeDiv.appendChild(pic)
 
-    let charNum = getCharNumber(person.url)
+    let pokeNum = getCharNumber(poke.url)
    
-    name.textContent = person.name
-    gender.textContent = person.gender
-    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    name.textContent = poke.name
+    type.textContent = poke.type
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${pokeNum}.jpg`
 
     
-    mainArea.appendChild(personDiv)
+    mainArea.appendChild(pokemonDiv)
 })*/
 
 var card = document.querySelector('.card');

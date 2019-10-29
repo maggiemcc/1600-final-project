2afd5f5d6b1548e1f121c4b8b4a3dbb6da8adf04
maggiemcc <maggiemccausland@hotmail.com>
2019-10-29 08:30:24 -0600

    changed picture

--- js/final.js
+++ js/final.js
@@ -1,3 +1,3 @@
 let myPic = document.createElement('img')
-myPic.setAttribute("src", 'http://www.horses-and-horse-information.com/images/american-paint-horse.jpg')
+myPic.setAttribute("src", 'https://vignette.wikia.nocookie.net/lotr/images/b/b6/Aragorn_profile.jpg/revision/latest?cb=20170121121423')
 document.body.appendChild(myPic)

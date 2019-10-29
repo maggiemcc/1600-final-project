417a6478a52647eb3ec619bd8cae36e768555064
maggiemcc <maggiemccausland@hotmail.com>
2019-10-29 11:12:05 -0600

    const all pokemon

--- js/baseball.js
+++ js/baseball.js
@@ -1,3 +1,28 @@
-import {species } from '../Assets/species'
-console.log(species)
 
+const allPokemon = (async () => {
+const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+const myJson = await response.json()
+console.log(JSON.stringify(myJson))
+})
+
+console.log(allPokemon().then)
+
+/*people.forEach(function(person) {
+    let personDiv = document.createElement('div')
+    let name = document.createElement('h1')
+    let gender = document.createElement('h3')
+    let pic = document.createElement('img')
+
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+    personDiv.appendChild(pic)
+
+    let charNum = getCharNumber(person.url)
+   
+    name.textContent = person.name
+    gender.textContent = person.gender
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+
+    
+    mainArea.appendChild(personDiv)
+})*/

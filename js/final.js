5559d7a3ae848669e04a2654eabd772b312eee33
maggiemcc <maggiemccausland@hotmail.com>
2019-10-29 08:55:23 -0600

    added grid

--- js/final.js
+++ js/final.js
@@ -1,3 +1,3 @@
 let myPic = document.createElement('img')
-myPic.setAttribute("src", 'https://vignette.wikia.nocookie.net/lotr/images/b/b6/Aragorn_profile.jpg/revision/latest?cb=20170121121423')
+myPic.setAttribute("src", 'https://lord-of-the-rings.org/books/Aragorn.jpg')
 document.body.appendChild(myPic)

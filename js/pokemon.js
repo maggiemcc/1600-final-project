b1a2190243608cf2a51dc487bc94ee57346f0039
maggiemcc <maggiemccausland@hotmail.com>
2019-10-31 18:01:10 -0600

    added images

--- js/pokemon.js
+++ js/pokemon.js
@@ -5,7 +5,7 @@ async function getAPIData(url) {
       const data = await response.json()
       return data
   } catch (error) {
-    console.log(error)
+    console.error(error)
 }
 }
 
@@ -15,7 +15,7 @@ const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
     for (const pokemon of data.results) {
       getAPIData(pokemon.url)
       .then(pokedata => {
-        console.log(pokedata)
+        populateDOM(pokedata)
       })
     }
 })
@@ -28,21 +28,20 @@ let mainArea = document.querySelector('main')
 function populateDOM(single_pokemon) {
     let pokeDiv = document.createElement('div')
     let name = document.createElement('h3')
-    let gender = document.createElement('p')
     let pic = document.createElement('img')
 
-    pokeDiv.setAttribute('class', 'charDivs')
-    pic.setAttribute('class', 'picDivs')
+    //pokeDiv.setAttribute('class', 'charDivs')
+   //pic.setAttribute('class', 'picDivs')
 
-    let pokeNum = getPokeNumber(single_pokemon.url)
-   
+    let pokeNum = getPokeNumber(single_pokemon.id)
+
     name.textContent = single_pokemon.name
 
     pic.src = '../images/${pokeNum}.png'
 
     pokeDiv.appendChild(name)
     pokeDiv.appendChild(pic)
-    pokeDiv.appendChild(gender)
+  
 
     mainArea.appendChild(pokeDiv)
   }

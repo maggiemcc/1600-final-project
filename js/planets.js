93a6806fa98aae5210267ba9523a5c6fee45d5e3
maggiemcc <maggiemccausland@hotmail.com>
2019-11-03 16:07:22 -0700

    added page for planets

---
+++ js/planets.js
@@ -0,0 +1,37 @@
+import { planets } from '../Assets/planets.js'
+
+
+let mainHeader = document.querySelector('header')
+let mainArea = document.querySelector('main')
+
+
+
+  
+planets.forEach(function(planet) {
+    let planetDiv = document.createElement('div')
+    let name = document.createElement('h1')
+    let pic = document.createElement('img')
+
+    planetDiv.appendChild(name)
+    planetDiv.appendChild(pic)
+
+    let charNum = getCharNumber(planet.url)
+   
+    name.textContent = planet.name
+    pic.src = `https://starwars-visualguide.com/assets/img/planets/${charNum}.jpg`
+
+    
+    mainArea.appendChild(planetDiv)
+})
+
+function getCharNumber(charURL) {
+  let end = charURL.lastIndexOf('/')
+  let charID = charURL.substring(end -2, end)
+  if(charID.indexOf('/') !== -1 ) {
+    return charID.slice(1,2)
+  } else {
+    return charID
+  }
+}
+
+const allDivs = Array.from(mainArea.querySelectorAll('div'))

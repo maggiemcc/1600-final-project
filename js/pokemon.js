64e9fbade743fac9e4a03b4ffbecccad661efd0c
maggiemcc <maggiemccausland@hotmail.com>
2019-11-05 22:42:47 -0700

    made Pokemon code appear on flip cards

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,4 +1,9 @@
 
+/* async function getPokemonData(url) {
+  const respond = await fetch(url)
+  return await response.json()
+} */
+
 async function getAPIData(url) {
   try {
       const response = await fetch(url)
@@ -9,7 +14,7 @@ async function getAPIData(url) {
 }
 }
 
-
+// now, use the return async data
 const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
 .then(data => { 
       for (const pokemon of data.results) {
@@ -26,29 +31,24 @@ let mainArea = document.querySelector('main')
 
 
 function populateDOM(single_pokemon) {
-    let pokeDiv = document.createElement('div')
+    let pokeScene = document.createElement('div')
     let pokeCard = document.createElement('div')
     let pokeFront = document.createElement('div')
     let pokeBack = document.createElement('div')
     let name = document.createElement('h3')
     let pic = document.createElement('img')
 
-  
-    fillCardBack(pokeBack, single_pokemon)
-
 
    pokeScene.setAttribute('class', 'scene')
    pokeCard.setAttribute('class', 'card')
-   pokeFront.setAttribute('class', 'charDivs card_face card_face-front')
-   pokeBack.setAttribute('class', 'card_face card_face-back')
+   pokeFront.setAttribute('class', 'charDivs card_face card_face--front')
+   pokeBack.setAttribute('class', 'card_face card_face--back')
    pic.setAttribute('class', 'picDivs')
 
 
     let pokeNum = getPokeNumber(single_pokemon.id)
     pokeFront.appendChild(name)
-    name.textContent = ${single_pokemon.name} height: ${single_pokemon.height}
-
-
+    name.textContent = `${single_pokemon.name} height: ${single_pokemon.height}`
 
     pic.src = `../images/${pokeNum}.png`
     pokeFront.appendChild(pic)
@@ -58,15 +58,19 @@ function populateDOM(single_pokemon) {
     pokeCard.appendChild(pokeBack)
     pokeScene.appendChild(pokeCard)
 
-    mainArea.appendChild(pokeScene)
-
-    pokeCard.addEvent
-   
-    pokeDiv.appendChild(pic)
-    pokeDiv.appendChild(name)
-  
-    mainArea.appendChild(pokeDiv)
+    mainArea.appendChild(pokeScene) 
+    
+    pokeCard.addEventListener( 'click', function() {
+      pokeCard.classList.toggle('is-flipped');
+    });
   }
+
+  {/* <div class="scene">
+<div class="card">
+<div class="card_face card_face--front">front</div>
+<div class="card_face card_face--back">back</div>
+</div>
+</div> */}
     
 
   function getPokeNumber(id) {

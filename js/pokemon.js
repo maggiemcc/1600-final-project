44be047625018a726580a3c5ea6ddc3be8f1ede2
maggiemcc <maggiemccausland@hotmail.com>
2019-11-21 14:01:51 -0700

    changed weight and height for Davemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -141,6 +141,6 @@ card.addEventListener( 'click', function() {
 
 // var myImage = new Image(100,200);
 // myImage.src = '../Images/900.png';
-// const Davemon = new Pokemon(810, 810, 'Davemon', 130, 28, 50);
+// const Davemon = new Pokemon(810, 810, 'Davemon', 20, 28, 50);
 // populateDOM(Davemon)
 // document.body.appendChild(myImage)

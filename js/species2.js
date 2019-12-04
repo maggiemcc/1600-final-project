ad0fa03f656dc2e648919a3e56e2f0fb5fd75307
maggiemcc <maggiemccausland@hotmail.com>
2019-12-04 16:37:41 -0700

    added more info on species

--- js/species2.js
+++ js/species2.js
@@ -5,13 +5,30 @@ let contentArea = document.querySelector('.content')
 species.forEach(species => {
     let speciesDiv = document.createElement('div')
     let speciesName = document.createElement('h3')
+    let speciesClassification = document.createElement('p')
+    let speciesLanguage = document.createElement('p')
+    let speciesAverage_height = document.createElement('p')
+    let speciesAverage_Lifespan = document.createElement('p')
+    let speciesDesignation = document.createElement('p')
     let speciesPic = document.createElement('img')
 
     speciesName.textContent = species.name
+    speciesDesignation.textContent = `Designation: ${species.designation}`
+    speciesClassification.textContent = `Classification: ${species.classification}`
+    speciesLanguage.textContent = `Language: ${species.language}`
+    speciesAverage_height.textContent = `Height(cm): ${species.average_height}`
+    speciesAverage_Lifespan.textContent = `Lifespan(yrs): ${species.average_lifespan}`
     let speciesNum= getCharNumber(species.url)
     speciesPic.src = `https://starwars-visualguide.com/assets/img/species/${speciesNum}.jpg`
-    
+
+
     speciesDiv.appendChild(speciesName)
+    speciesDiv.appendChild(speciesDesignation)
+    speciesDiv.appendChild(speciesClassification)
+    speciesDiv.appendChild(speciesLanguage)
+    speciesDiv.appendChild(speciesAverage_height)
+    speciesDiv.appendChild(speciesAverage_Lifespan)
+
     speciesDiv.appendChild(speciesPic)
     contentArea.appendChild(speciesDiv)
 })
